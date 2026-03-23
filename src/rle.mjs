7cf7865137a0d8132e51cb50c79a6592f1e82f06
Roni Tuohino <rt@ronituohino.com>
2026-03-23 11:19:59 +0200

    Decoding rle file is more complicated, try another approach

--- src/rle.mjs
+++ src/rle.mjs
@@ -1,4 +1,5 @@
 import { readFileSync } from "node:fs";
+import { Pattern } from "./pattern.mjs";
 
 export class RLE {
   static fromFile(path) {
@@ -12,5 +13,79 @@ export class RLE {
 
   toString() {
     return this.contents;
+  }
+  toPattern() {
+    let sizeX;
+    let sizeY;
+    const characters = [];
+
+    const lines = this.contents.split("\n");
+    for (let l = 0; l < lines.length; l++) {
+      const line = lines[l];
+      if (line.startsWith("#")) {
+        continue;
+      }
+      if (line.startsWith("x")) {
+        const headers = line.split(",");
+        const parsedHeaders = {};
+        headers.forEach((header) => {
+          const [key, value] = header.trim().split("=");
+          parsedHeaders[key.trim()] = parseInt(value.trim()) || value.trim();
+        });
+
+        sizeX = parsedHeaders.x;
+        sizeY = parsedHeaders.y;
+        continue;
+      }
+
+      // Shape data
+      let runningCountChars = [];
+      let runningCount = 1;
+      let lineCharacterCount = 0;
+      for (let c = 0; c < line.length; c++) {
+        const character = line[c];
+
+        switch (character) {
+          case "b":
+            // dead cell
+            characters.push(".".repeat(runningCount).split(""));
+            lineCharacterCount += runningCount;
+            runningCount = 1;
+            runningCountChars = [];
+            continue;
+          case "o":
+            // alive cell
+            characters.push("x".repeat(runningCount).split(""));
+            lineCharacterCount += runningCount;
+            runningCount = 1;
+            runningCountChars = [];
+            continue;
+          case "$":
+            // new line
+            for (let i = 0; i < runningCount; i++) {
+              characters.push(".".repeat(sizeX - lineCharacterCount).split(""));
+              lineCharacterCount = 0;
+            }
+            runningCount = 1;
+            runningCountChars = [];
+
+            continue;
+          case "!":
+            break;
+          default:
+            // number
+            if (parseInt(character) !== NaN) {
+              runningCountChars.push(character);
+              runningCount = parseInt(runningCountChars.join(""));
+            }
+            continue;
+        }
+      }
+    }
+
+    const finalString = [];
+    characters.forEach((part) => finalString.push(part.join("")));
+
+    return new Pattern(finalString.join(""), sizeX, sizeY);
   }
 }
--- test/rle.test.mjs
+++ test/rle.test.mjs
@@ -1,11 +1,29 @@
-import { describe, test } from "vitest";
+import { beforeEach, describe, test } from "vitest";
 import { expect } from "chai";
 import { RLE } from "../src/rle.mjs";
+import { cleanString } from "../src/utils.mjs";
 
 describe("RLE", () => {
-  test("can read a .rle file", () => {
-    expect(RLE.fromFile("./test/shapes/gosper_glider_gun.rle").toString().startsWith("#N Gosper glider gun")).to.be
-      .true;
+  let rle;
+  beforeEach(() => {
+    rle = RLE.fromFile("./test/shapes/gosper_glider_gun.rle");
   });
-  test("can produce a Pattern", () => {});
+  test("can read a .rle file", () => {
+    expect(rle.toString().startsWith("#N Gosper glider gun")).to.be.true;
+  });
+  test.skip("can produce a Pattern", () => {
+    expect(rle.toPattern().characters).to.deep.equal(
+      cleanString(`
+        ........................x...........
+        ......................x.x...........
+        ............xx......xx............xx
+        ...........x...x....xx............xx
+        xx........x.....x...xx..............
+        xx........x...x.xx....x.x...........
+        ..........x.....x.......x...........
+        ...........x...x....................
+        ............xx......................
+      `),
+    );
+  });
 });
